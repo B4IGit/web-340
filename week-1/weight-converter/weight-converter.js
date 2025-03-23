bb638dfc76851c4cf4a217757652e2236f6d2694
Devin Ledesma <dev.dfledesma@gmail.com>
2025-03-22 19:22:57 -0500

    added config and ignore file

---
+++ week-1/weight-converter/weight-converter.js
@@ -0,0 +1,10 @@
+/**
+ * Author: Devin Ledesma
+ * Date: 03/22/2025
+ * File Name: Weight Converter
+ * Description: Converts lbs to kil
+ */
+
+"use strict";
+
+// TODO: Implement the weight conversion logic here
