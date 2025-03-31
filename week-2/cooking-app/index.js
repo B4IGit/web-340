7f02dfe83396e38dbffc8c3a46ba3a660e1d3e77
Devin Ledesma <dev.dfledesma@gmail.com>
2025-03-30 20:19:50 -0500

    ready for submittion

--- week-2/cooking-app/index.js
+++ week-2/cooking-app/index.js
@@ -1,10 +1,17 @@
 /**
- * Author:
- * Date:
- * File Name:
- * Description:
+ * Author:      Devin Ledesma
+ * Date:        03/30/2025
+ * File Name:   index.js
+ * Description: This file imports three functions from recipes.js by logging and returning the values of each
 */
+"use strict";
 
 // TODO: Import your module using require
+const { createRecipe, setTimer, quit } = require('./recipes');
 
 // TODO: Implement your CLI program here
+const ingredients = ["Chicken", "Noodles"]
+
+console.log(createRecipe(ingredients));
+console.log(setTimer(15));
+console.log(quit());
--- week-2/cooking-app/recipes.js
+++ week-2/cooking-app/recipes.js
@@ -1,23 +1,44 @@
 /**
- * Author:
- * Date:
- * File Name:
- * Description:
+ * Author:      Devin Ledesma
+ * Date:        03/30/2025
+ * File Name:   recipes.js
+ * Description: This file styles the output for each function: createRecipe(ingredients), setTimer(minutes), and quit()
 */
+"use strict";
 
 // Define the createRecipe function
 function createRecipe(ingredients) {
-  // TODO: Implement this function
+  // create formated message for list of ingredients
+  let ingredientMsg = 'Recipe created with ingredients: ';
+
+  // loop through the ingredients - adds comma to separate each ingredient
+  for (let i = 0; i < ingredients.length; i++) {
+    // used AI for assistance to separate each ingredient with a comma
+    ingredientMsg += ingredients[i];
+    if (i < ingredients.length - 1) {
+      ingredientMsg += ', ';
+    }
+  }
+
+  return ingredientMsg
 }
 
 // Define the setTimer function
 function setTimer(minutes) {
   // TODO: Implement this function
+  return `Timer set for ${minutes} minutes`;
 }
 
 // Define the quit function
 function quit() {
   // TODO: Implement this function
+  return `Program exited`;
 }
 
+
 // TODO: Export the functions
+module.exports = {
+  createRecipe: createRecipe,
+  setTimer: setTimer,
+  quit: quit,
+};
