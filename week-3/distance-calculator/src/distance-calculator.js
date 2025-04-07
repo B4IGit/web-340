5a393c72928f5dd26c09c7e5eac8235849a87b4a
Devin Ledesma <dev.dfledesma@gmail.com>
2025-04-06 20:05:38 -0500

    Ready to Submit

--- week-3/distance-calculator/src/distance-calculator.js
+++ week-3/distance-calculator/src/distance-calculator.js
@@ -1,5 +1,26 @@
+"use strict";
+
+// array to store the distance of each planet from the sun
+const planets = {
+  'mercury': 0.39,
+  'venus': 0.72,
+  'earth': 1,
+  'mars': 1.52,
+  'jupiter': 5.20,
+  'saturn': 9.54,
+  'uranus': 19.22,
+  'neptune': 30.06,
+}
 function calculateDistance(planet1, planet2) {
-  // TODO: Implement this function
+  // access planet from planets array
+  let distanceOfPlanet1 = planets[planet1];
+  let distanceOfPlanet2 = planets[planet2];
+
+  // returns the difference between planet1 and planet2
+  return Math.abs(distanceOfPlanet2 - distanceOfPlanet1);
 }
 
-module.exports = calculateDistance;
+module.exports = {
+  planets,
+  calculateDistance
+};
--- week-3/distance-calculator/test/distance-calculator.spec.js
+++ week-3/distance-calculator/test/distance-calculator.spec.js
@@ -1,8 +1,48 @@
-const assert = require('assert');
-const calculateDistance = require('../src/distance-calculator');
+"use strict";
 
-function testFunctionDescription() {
-  // TODO: Implement this function
+const assert = require('assert');
+const {planets, calculateDistance} = require("../src/distance-calculator");
+
+// test 1
+function testPlanetDistance() {
+  try {
+    let getDistance = planets.mercury;
+    assert.strictEqual(getDistance, 0.39);
+    console.log('testPlanetDistance passed');
+    return true;
+  } catch (error) {
+    console.error(`Failed testPlanetDistance: ${error.message}`);
+    return false;
+  }
+}
+
+// test 2
+function testSaturnToNeptune() {
+  try {
+     let distance = calculateDistance('saturn', 'neptune');
+     assert.strictEqual(distance, 20.52);
+     console.log('testSaturnToNeptune passed');
+     return true;
+  } catch (error) {
+     console.error(`Failed testSaturnToNeptune: ${error.message}`);
+     return false;
+  }
+}
+
+// test 3
+function testEarthToUranus() {
+  try {
+    let distance = calculateDistance('earth', 'uranus');
+    assert.equal(distance, 18.22);
+    console.log('testEarthToUranus passed');
+    return true;
+  } catch (error) {
+    console.error(`Failed testEarthToUranus: ${error.message}`);
+    return false;
+  }
 }
 
 // Call your test functions here
+testPlanetDistance();
+testSaturnToNeptune();
+testEarthToUranus();
