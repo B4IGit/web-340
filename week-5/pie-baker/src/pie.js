0285b86cac69919459ee117167da9fec8a18441b
Devin Ledesma <dev.dfledesma@gmail.com>
2025-04-29 20:33:55 -0500

    Ready to Submit

--- week-5/pie-baker/src/pie.js
+++ week-5/pie-baker/src/pie.js
@@ -1,13 +1,31 @@
 /**
- * Author:
- * Date:
- * File Name:
- * Description:
+ * Author:      Devin Ledesma
+ * Date:        04/29/25
+ * File Name:   pie.js
+ * Description: Exports the bakePie function which validates the pie type and its essential ingredients, logs error and success messages to console.
  */
 "use strict";
 
 function bakePie(pieType, ingredients) {
-  // Your code here
-}
+  // Check if pieType is declared
+  if (!pieType) {
+    console.log('Error: Pie Type is undefined!');
+    process.exit(1);
+  }
 
+  // Declare essential ingredients
+  const essentialIngredient = ['flour', 'sugar', 'butter'];
+
+  // Checks if essential ingredients are included
+  const hasAllIngredients = essentialIngredient.every(i => ingredients.includes(i)); // had some help with AI
+
+  if (!hasAllIngredients) {
+    // Exits the process if missing essential ingredients
+    console.log('Missing essential ingredient: Your pie cannot be baked.');
+    process.exit(1);
+  } else {
+    // Display success message if all essential ingredients are included
+    console.log(`${pieType} Pie was baked successfully!`);
+  }
+}
 module.exports = { bakePie };
--- week-5/pie-baker/test/pie.spec.js
+++ week-5/pie-baker/test/pie.spec.js
@@ -1,8 +1,8 @@
 /**
- * Author:
- * Date:
- * File Name:
- * Description:
+ * Author:      Devin Ledesma
+ * Date:        04/29/25
+ * File Name:   pie.spec.js
+ * Description: Using TDD and jest for unit testing.
  */
 
 "use strict";
@@ -10,3 +10,34 @@
 const { bakePie } = require("../src/pie");
 
 // Your tests here
+const exit = jest.spyOn(process, 'exit').mockImplementation((code) => code);
+
+describe('bakePie', () => {
+  let log;
+
+  beforeEach(() => {
+    log = jest.spyOn(console, "log");
+  });
+
+  afterEach(() => {
+    log.mockRestore();
+  });
+
+  test('should bake successfully if all essential ingredient are declared', () => {
+    bakePie('Apple', ['flour', 'sugar', 'butter']);
+    expect(log).toHaveBeenCalledWith('Apple Pie was baked successfully!');
+    expect(exit).not.toHaveBeenCalled();
+  });
+
+  test('should console log error message and exit if essential ingredient is missing', () => {
+    bakePie('Cherry', ['cherry', 'flour', 'sugar']);
+    expect(log).toHaveBeenCalledWith('Missing essential ingredient: Your pie cannot be baked.');
+    expect(exit).toHaveBeenCalledWith(1);
+  });
+
+  test('should exit process and display console log if pieType is not declared', () => {
+    bakePie('', ['flour', 'sugar', 'butter']);
+    expect(log).toHaveBeenCalledWith('Error: Pie Type is undefined!');
+    expect(exit).toHaveBeenCalledWith(1);
+  });
+})
