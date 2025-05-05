b33f4e58d5d7fb8cf143b5e597759fbb056a76a0
Devin Ledesma <dev.dfledesma@gmail.com>
2025-05-04 21:34:38 -0500

    Ready to Submit

--- week-7/fantasy-character-creation-stream/src/character-creator.js
+++ week-7/fantasy-character-creation-stream/src/character-creator.js
@@ -4,14 +4,42 @@ class CharacterCreator extends Duplex {
   constructor(options) {
     super(options);
     // TODO: Initialize your class here
+    this.queue = [];
   }
 
   _write(chunk, encoding, callback) {
     // TODO: Implement your _write method here
+    const data = chunk.toString().trim();
+
+    // Checks if data is empty
+    if (!data) {
+      callback(new Error('No data provided.'));
+      return;
+    }
+
+    try {
+      // Parse the character data
+      const character = JSON.parse(data);
+
+      // Format character data
+      const formatData = `A ${character.gender} ${character.class} ${character.funFact}.`;
+
+      // Add to queue
+      this.queue.push(formatData);
+
+      callback();
+    } catch (error) {
+      callback(error);
+    }
   }
 
   _read(size) {
     // TODO: Implement your _read method here
+    if (this.queue.length) {
+      this.push(this.queue.shift());
+    } else {
+      this.push(null);
+    }
   }
 }
 
--- week-7/fantasy-character-creation-stream/test/character-creator.spec.js
+++ week-7/fantasy-character-creation-stream/test/character-creator.spec.js
@@ -9,13 +9,48 @@ describe('CharacterCreator', () => {
 
   test("should process data correctly when written to", (done) => {
     // TODO: Write your test here
+    const character = JSON.stringify({
+      class: 'warrior',
+      gender: 'male',
+      funFact: 'has the power of invisibility'
+    });
+
+    characterCreator.on('data', (data) => {
+      const output = data.toString().trim();
+      const expectedOutput = 'A male warrior has the power of invisibility.';
+
+      expect(output).toBe(expectedOutput);
+      done();
+    });
+
+    characterCreator.write(character);
   });
 
   test("should emit 'error' when invalid data is written", (done) => {
     // TODO: Write your test here
+    characterCreator.on('error', (err) => {
+      expect(err.message).toBe('No data provided.');
+      done();
+    });
+
+    characterCreator.write('');
   });
 
   test("should transform data correctly when written to", (done) => {
     // TODO: Write your test here
+    const character = JSON.stringify({
+      class: 'mage',
+      gender: 'female',
+      funFact: 'has the power to summon dark souls'
+    });
+
+    const expectedOutput = 'A female mage has the power to summon dark souls.';
+
+    characterCreator.write(character, 'utf8', () => {
+      characterCreator.on('data', (data) => {
+        expect(data.toString().trim()).toBe(expectedOutput);
+        done();
+      })
+    })
   });
 });
